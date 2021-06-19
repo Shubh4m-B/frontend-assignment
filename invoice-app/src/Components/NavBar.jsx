4a8c6c39472db7cbd1b4a69b6fb7ca21ac16754c
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-06-19 19:49:42 +0530

    Style the navbar

--- invoice-app/src/Components/NavBar.jsx
+++ invoice-app/src/Components/NavBar.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import '../Styles/NavBar.css'
 
 export class NavBar extends Component {
     render() {
         return (
-            <div>
-                <h1>NavBar</h1>
+            <div className="NavBar">
+                <h1 className="NavBar-brand">Product Name</h1>
+                <ul className="NavBar-list">
+                    <li className="NavBar-item">
+                        My Orders
+                    </li>
+                    <li className="NavBar-item">
+                        My Cart
+                    </li>
+                    <li className="NavBar-item">
+                        Logout
+                    </li>
+                </ul>
             </div>
         )
     }
